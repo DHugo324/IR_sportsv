f5a63a8d0a4335e076c3042526d0ea4c54fa8e85
DHugo324 <116426935+DHugo324@users.noreply.github.com>
2025-04-14 20:59:57 +0800

    separate executable file

--- webCrawler/article_crawler.py
+++ webCrawler/article_crawler.py
@@ -2,63 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import re
-import json
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 }
-
-MAX_PAGES = 2 # 爬取的頁數
-OUTPUT_FILE = "basketball_articles.json" # 儲存的檔案名稱
-all_articles = []  # 儲存所有文章資料
-
-def get_basketball_article_id_from_page(page_num): # 爬取籃球專區的文章ID
-    url = f'https://www.sportsv.net/basketball?page={page_num}#latest_articles'
-    try:
-        res = requests.get(url, headers=HEADERS, timeout=10)
-        res.raise_for_status()
-    except requests.RequestException as e:
-        print(f"  第 {page_num} 頁請求失敗：{e}")
-        return []
-    soup = BeautifulSoup(res.text, 'html.parser')
-    latest_articles_section = soup.find('section', id='latest_articles')
-    articles = latest_articles_section.find_all('div', class_='col-md-7')
-
-    ids = [] # 儲存文章id
-    for article in articles:
-        h3_tag = article.find('h3')
-        if h3_tag:
-            link_tag = h3_tag.find('a')
-            if link_tag and link_tag.get('href'):
-                link = link_tag.get('href')
-                id = get_article_id(link) # 文章ID
-                # if link.startswith('/'):
-                #     link = "https://www.sportsv.net" + link
-                ids.append(id)
-
-    return ids
-
-def get_basketball_article_links(page_num): # 爬取籃球專區的文章連結
-    url = f'https://www.sportsv.net/basketball?page={page_num}#latest_articles'
-    soup = get_article_soup(url) # 獲取頁面內容
-    if soup is None:
-        print(f"  第 {page_num} 頁請求失敗")
-        return []
-    latest_articles_section = soup.find('section', id='latest_articles')
-    articles = latest_articles_section.find_all('div', class_='col-md-7')
-
-    links = [] # 儲存文章網址和日期
-    for article in articles:
-        h3_tag = article.find('h3')
-        if h3_tag:
-            link_tag = h3_tag.find('a')
-            if link_tag and link_tag.get('href'):
-                link = link_tag.get('href')
-                if link.startswith('/'):
-                    link = "https://www.sportsv.net" + link
-                links.append(link)
-
-    return links
 
 def get_article_soup(url): # 獲取文章內容的 BeautifulSoup 物件
     try:
@@ -157,35 +104,3 @@ def get_article_content(url): # 獲取文章內容
         "tags": tags,
         "article-content": full_content.strip() if full_content else None
     }
-
-def save_articles_to_json(data_list, filename):
-    with open(filename, "w", encoding="utf-8") as file:
-        json.dump(data_list, file, indent=4, ensure_ascii=False)
-
-def main():
-    global all_articles
-    print(f"開始爬取 {MAX_PAGES} 頁的文章...")
-
-    for i in range(1, MAX_PAGES + 1):
-        print(f" 正在抓取第 {i} 頁文章連結...")
-
-        links = get_basketball_article_links(i)
-
-        if not links:
-            print(f" 第 {i} 頁沒有找到文章，跳過。")
-            continue
-
-        for index, url in enumerate(links, start=1):
-                print(f" [{index}/{len(links)}] 爬取中：{url})")
-                article_data = get_article_content(url)
-                if article_data and article_data["article-content"]:
-                    all_articles.append(article_data)
-                    print(f"   完成：{url}")
-                else:
-                    print(f"   失敗：{url}")
-
-    save_articles_to_json(all_articles, OUTPUT_FILE)
-    print(f"✅ 所有符合條件的文章已存入 {OUTPUT_FILE}")
-
-if __name__ == "__main__":
-    main()
